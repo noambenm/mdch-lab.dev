a6679fc403b92e3b1f5605cd315cf84cfa94a8a0
Noam Ben Mordechai <noam112113@gmail.com>
2025-03-15 02:49:43 +0200

    Add Hubble Ingress resource and remove cert-manager Helm script

--- k8s/cert-manager/helm.sh
+++
@@ -1,7 +0,0 @@
-helm repo add jetstack https://charts.jetstack.io --force-update
-helm install \
-  cert-manager jetstack/cert-manager \
-  --namespace cert-manager \
-  --create-namespace \
-  --version v1.17.0 \
-  --set crds.enabled=true
