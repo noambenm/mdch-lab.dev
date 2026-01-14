c0caac5c2e4e7ade5b1a59a0c0c434bda1f336dd
noambe <noambe@comm-it.com>
2026-01-14 10:43:03 +0200

    removed cb sa name

--- terraform/im-workspace.tf
+++ terraform/im-workspace.tf
@@ -4,12 +4,10 @@ module "im-workspace" {
   location                   = var.region
   trigger_location           = var.region
   project_id                 = var.project_id
-  repo_connection_name       = "im-mdch-lab"
   deployment_id              = var.deployment_id
   im_deployment_repo_uri     = var.im_deployment_repo_uri
   im_deployment_ref          = "main"
   infra_manager_sa           = var.infra_manager_sa
-  custom_cloudbuild_sa_name  = "cb-sa-mdch-lab"
   github_app_installation_id = var.github_app_installation_id
   github_pat_secret          = var.github_pat_secret
 }
