c99ff854cd9fdbbf86f9a1c68b3603b9261cb78a
noambe <noambe@comm-it.com>
2026-01-14 11:24:11 +0200

    added tf version

--- terraform/im-workspace.tf
+++ terraform/im-workspace.tf
@@ -5,6 +5,7 @@ module "im-workspace" {
   trigger_location           = var.region
   project_id                 = var.project_id
   deployment_id              = var.deployment_id
+  tf_version                 = "1.5.7"
   im_deployment_repo_uri     = var.im_deployment_repo_uri
   im_deployment_ref          = "main"
   infra_manager_sa           = var.infra_manager_sa
